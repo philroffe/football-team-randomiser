8af7721e5474d79a44610da4f46943febb789ff5
Phil Roffe <philroffe@gmail.com>
2022-08-31 23:23:40 +0100

    Added new branch migrate-from-postgres-to-google-datastore - partial commit to share working

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ const pool = new Pool({
   }
 });
 
+
+// By default, the client will authenticate using the service account file
+// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
+// the project specified by the GOOGLE_CLOUD_PROJECT environment variable. See
+// https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
+// These environment variables are set automatically on Google App Engine
+const {Datastore} = require('@google-cloud/datastore');
+// Instantiate a datastore client
+const datastore = new Datastore({
+  projectId: 'long-door-651',
+});
+
 // store a cache of the data for X seconds
 // useful to allow a quick refresh of the screen to randomise players
 var nextMonday = new Date();
@@ -25,6 +37,29 @@ express()
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
 .get('/', (req, res) => res.render('pages/index'))
+.get('/db', async (req, res, next) => {
+  // Create a visit record to be stored in the database
+  const visit = {
+    timestamp: new Date(),
+    // Store a hash of the visitor's ip address
+    userIp: req.ip,
+  };
+
+  try {
+    await insertVisit(visit);
+    const [entities] = await getVisits();
+    const visits = entities.map(
+      entity => `Time: ${entity.timestamp}, AddrHash: ${entity.userIp}`
+    );
+    res
+      .status(200)
+      .set('Content-Type', 'text/plain')
+      .send(`Last 10 visits:\n${visits.join('\n')}`)
+      .end();
+  } catch (error) {
+    next(error);
+  }
+})
 .get('/teams', async (req, res) => {
       try {
         console.log('Rendereing TEAMS page with data');
@@ -80,17 +115,28 @@ express()
         var requestedDateMonth = requestedDate.toISOString().split('T')[0]
         //console.log("PPP=" + monthDateNumericFormat.format(requestedDate))
 
-        const client = await pool.connect();
-        const dbresult = await client.query('SELECT * FROM games WHERE gameid=\'' + requestedDateMonth + '\'');
-        //console.log('dbresult=' + JSON.stringify(dbresult));
+
+const allquery = datastore.createQuery("games");
+const [alldbresult] =  await datastore.runQuery(allquery);
+console.log('alldbresult=' + JSON.stringify(alldbresult));
+    //const [dbresult] = await getGames();
+    const query = datastore.createQuery("games").filter('gameid', '=', requestedDateMonth).order('timestamp', {descending: true}).limit(1);
+    const [dbresult] =  await datastore.runQuery(query);
+
+//        const client = await pool.connect();
+//        const dbresult = await client.query('SELECT * FROM games WHERE gameid=\'' + requestedDateMonth + '\'');
+        console.log('dbresult=' + JSON.stringify(dbresult));
 
         //if (!rowdata) {
-        if (dbresult.rowCount == 1) {
-          rowdata = dbresult.rows[0];
+        if (dbresult[0]) {
+          //if (dbresult.rowCount == 1) {
+          //rowdata = dbresult.rows[0];
+          rowdata = dbresult[0]
         } else {
           //rowdata = { "status": "NEW", "pollYear": "2022", "pollMonth": "02", "players": {} };
           // create a blank entry to render the poll page
-          rowdata = { "status": "NO_DATABASE_ENTRY", "gameid": requestedDateMonth, "gamedetails":{"players":{}}}
+          //rowdata = { "status": "NO_DATABASE_ENTRY", "gameid": requestedDateMonth, "gamedetails":{"players":{}}}
+          rowdata = { "status": "NO_DATABASE_ENTRY", "gameid": requestedDateMonth, "players":{}}
         }
         console.log('rowdata=' + JSON.stringify(rowdata));
 
@@ -100,7 +146,7 @@ express()
         var pageData = { gameInfo: gameInfo, data: rowdata };
         res.render('pages/poll', { pageData: pageData} );
         //res.render('pages/poll', { gameInfo: JSON.stringify(gameInfo), data: pageData } );
-        client.release();
+//        client.release();
       } catch (err) {
         console.error(err);
         res.send("Error " + err);
@@ -113,12 +159,14 @@ express()
     var players = req.body.players;
     
     gameId = gameYear + "-" + gameMonth + "-01";
-    var gamedetails_new = { "pollYear": gameYear, "pollMonth": gameMonth, "players": players };
+    var timestamp = new Date();
+    var gamedetails_new = { "gameid": gameId, "timestamp": timestamp, "pollYear": gameYear, "pollMonth": gameMonth, "players": players };
     gamedetailsNewJson = JSON.stringify(gamedetails_new);
     console.log('Inserting DB data:', gamedetailsNewJson);
 
-
     try {
+      
+/**
       ////// TODO Database LOCK?
       const client = await pool.connect();
       const dbresult = await client.query('SELECT * FROM games WHERE gameid=\'' + gameId + '\'');
@@ -162,11 +210,15 @@ express()
         '${gamedetailsNewJson}', '${gamedetailsMergedJson}', '${gamedetailsDiffJson}');`)
       const historyresults = { 'historyresults': (historyresult) ? historyresult.rows : null};
       console.log('Got History DB results:', historyresults);
+**/
+
+      //datastore.save({ key: gameId, data: gamedetails_new });
+      await datastore.save({ key: datastore.key('games'), data: gamedetails_new})
 
       // if you got here without an exception then everything was successful
       //res.sendStatus(200);
       res.json({'result': 'OK'})
-      client.release();
+//      client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -183,4 +235,37 @@ function createJsonDiff(obj1, obj2) {
     }
   }
   return ret;
+};
+
+
+/**
+ * Insert a visit record into the database.
+ *
+ * @param {object} visit The visit record to insert.
+ */
+const insertVisit = visit => {
+  return datastore.save({
+    key: datastore.key('visit'),
+    data: visit,
+  });
+};
+
+/**
+ * Retrieve the latest 10 visit records from the database.
+ */
+const getVisits = () => {
+  const query = datastore
+    .createQuery('visit')
+    .order('timestamp', {descending: true})
+    .limit(10);
+
+  return datastore.runQuery(query);
+};
+
+/**
+ * Retrieve the latest 10 games records from the database.
+ */
+const getGame = gameId => {
+  const query = datastore.createQuery(gameId);
+  return datastore.runQuery(query);
 };
